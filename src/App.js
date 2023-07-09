fc54437292d92431234364c035d283e7beee7b60
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-08 21:39:53 -0400

    Create state to store team names and whether or not they have been clicked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [teams, setTeams] = useState([
+    {teamName: 'Real Madrid', clicked: false,}, 
+    {teamName: 'Milan', clicked: false,},
+    {teamName: 'Liverpool', clicked: false,},
+    {teamName: 'Bayern', clicked: false,},
+    {teamName: 'Barcelona', clicked: false,},
+    {teamName: 'Ajax', clicked: false,},
+    {teamName: 'Manchester United', clicked: false,},
+    {teamName: 'Inter', clicked: false,},
+    {teamName: 'Chelsea', clicked: false,},
+    {teamName: 'Juventus', clicked: false,},
+    {teamName: 'Porto', clicked: false,},
+    {teamName: 'Manchester City', clicked: false,},
+    {teamName: 'Dortmund', clicked: false,},
+    {teamName: 'Feyenoord', clicked: false,},
+    {teamName: 'Benfica', clicked: false,},
+    {teamName: 'Aston Villa', clicked: false,},
+    {teamName: 'Crvena Zvezda', clicked: false,},
+    {teamName: 'PSV', clicked: false,},
+    {teamName: 'Celtic', clicked: false,},
+  ])
+
   return (
     <div>
-      
+    
     </div>
   );
 }
