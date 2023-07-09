8340f1eb4c57d6a382cfccccf16ccb4c63576706
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-08 22:11:48 -0400

    Import uniqid and add an id attribute to each team

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
+import uniqid from 'uniqid'
 import Cards from "./components/cards";
 
 function App() {
 
   const [teams, setTeams] = useState([
-    {teamName: 'Real Madrid', clicked: false,}, 
-    {teamName: 'Milan', clicked: false,},
-    {teamName: 'Liverpool', clicked: false,},
-    {teamName: 'Bayern', clicked: false,},
-    {teamName: 'Barcelona', clicked: false,},
+    {teamName: 'Real Madrid', clicked: false, id: uniqid(),}, 
+    {teamName: 'Milan', clicked: false, id: uniqid(),},
+    {teamName: 'Liverpool', clicked: false, id: uniqid(),},
+    {teamName: 'Bayern', clicked: false, id: uniqid(),},
+    {teamName: 'Barcelona', clicked: false, id: uniqid(),},
     {teamName: 'Ajax', clicked: false,},
-    {teamName: 'Manchester United', clicked: false,},
-    {teamName: 'Inter', clicked: false,},
-    {teamName: 'Chelsea', clicked: false,},
-    {teamName: 'Juventus', clicked: false,},
+    {teamName: 'Manchester United', clicked: false, id: uniqid(),},
+    {teamName: 'Inter', clicked: false, id: uniqid(),},
+    {teamName: 'Chelsea', clicked: false, id: uniqid(),},
+    {teamName: 'Juventus', clicked: false, id: uniqid(),},
     {teamName: 'Porto', clicked: false,},
-    {teamName: 'Manchester City', clicked: false,},
-    {teamName: 'Dortmund', clicked: false,},
-    {teamName: 'Feyenoord', clicked: false,},
-    {teamName: 'Benfica', clicked: false,},
-    {teamName: 'Aston Villa', clicked: false,},
-    {teamName: 'Crvena Zvezda', clicked: false,},
-    {teamName: 'PSV', clicked: false,},
-    {teamName: 'Celtic', clicked: false,},
+    {teamName: 'Manchester City', clicked: false, id: uniqid(),},
+    {teamName: 'Dortmund', clicked: false, id: uniqid(),},
+    {teamName: 'Feyenoord', clicked: false, id: uniqid(),},
+    {teamName: 'Benfica', clicked: false, id: uniqid(),},
+    {teamName: 'Aston Villa', clicked: false, id: uniqid(),},
+    {teamName: 'Crvena Zvezda', clicked: false, id: uniqid(),},
+    {teamName: 'PSV', clicked: false, id: uniqid(),},
+    {teamName: 'Celtic', clicked: false, id: uniqid(),},
   ])
 
   return (
