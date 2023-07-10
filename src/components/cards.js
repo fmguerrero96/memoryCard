795f273924c0d50cda367f68f54754a67f4cdac6
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-10 14:33:00 -0400

    Move onClick prop to main card div

--- src/components/cards.js
+++ src/components/cards.js
@@ -4,8 +4,8 @@ export default function Cards({teams, shuffleTeams}) {
     return(
         <div className="cardsContainer">
             {teams.map((team) => (
-                <div className="card" key={team.id}>
-                    <img onClick={shuffleTeams} className="badge" src={team.badge}/>
+                <div className="card" key={team.id} onClick={shuffleTeams}>
+                    <img className="badge" src={team.badge}/>
                     <div className="teamName">{team.teamName}</div>
                 </div>
             ))}
