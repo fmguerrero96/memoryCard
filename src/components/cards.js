26c5d086693d8333232adbcca95ec1b6aa95ea93
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-10 11:30:43 -0400

    Remove unnecessary div and add class names to elements

--- src/components/cards.js
+++ src/components/cards.js
@@ -3,14 +3,12 @@ import React from "react";
 export default function Cards({teams}) {
     return(
         <div className="cardsContainer">
-            <div>
-                {teams.map((team) => (
-                    <div key={team.id}>
-                        <img src={team.badge}/>
-                        <div>{team.teamName}</div>
-                    </div>
-                ))}
-            </div>
+            {teams.map((team) => (
+                <div className="card" key={team.id}>
+                    <img className="badge" src={team.badge}/>
+                    <div className="teamName">{team.teamName}</div>
+                </div>
+            ))}
         </div>
     )
 }
