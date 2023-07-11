ac85cb161a2278d2d5e5bc0b596216191585b8cb
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-11 19:21:54 -0400

    Pass in team.id as a parameter to shuffleTeams function

--- src/components/cards.js
+++ src/components/cards.js
@@ -4,7 +4,7 @@ export default function Cards({teams, shuffleTeams}) {
     return(
         <div className="cardsContainer">
             {teams.map((team) => (
-                <div className="card" key={team.id} onClick={shuffleTeams}>
+                <div className="card" key={team.id} onClick={ () => { shuffleTeams(team.id);} }>
                     <img className="badge" src={team.badge}/>
                     <div className="teamName">{team.teamName}</div>
                 </div>
