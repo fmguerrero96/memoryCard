ed7c19b50be2ccf4db553a17bab39f0fc15854eb
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-12 13:29:06 -0400

    Import useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import uniqid from 'uniqid'
 import _ from 'lodash'
 import Cards from "./components/cards";
