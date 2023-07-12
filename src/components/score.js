fb8e18a76e51b11f4fc52338cfcb64d7f2cf21b2
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-12 15:53:25 -0400

    Include 'clickedTeams' state and useEffect hoook to monitor currentScore

--- src/components/score.js
+++ src/components/score.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-export default function Score() {
+export default function Score({teams}) {
+    const [clickedTeams, setClickedTeams] = useState([]);
+
+    useEffect(() => {
+        const updatedClickedTeams = teams.filter((team) => team.clicked === true);
+        setClickedTeams(updatedClickedTeams);
+      }, [teams]);
+
     return(
         <div className="scoreBoard">
-            <div className="currentScore"></div>
-            <div className="highScore"></div>
+            <div className="currentScore">{clickedTeams.length}</div>
+            <div className="highScore">10</div>
         </div>
     )
 }
