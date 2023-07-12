1513082e8ac2e3b8e773b6c7c32e9110b7d1666c
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-12 13:05:49 -0400

    Create score componenet

---
+++ src/components/score.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Score() {
+    return(
+        <div className="scoreBoard">
+            <div className="currentScore"></div>
+            <div className="highScore"></div>
+        </div>
+    )
+}
