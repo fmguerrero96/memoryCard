a06548c77db2b32230ea54838e26e91d6f7d8fd3
Misael Guerrero <mguerrerowest@gmail.com>
2023-07-12 20:17:21 -0400

    Create new state variable 'highScore', and update highScore functionality

--- src/components/score.js
+++ src/components/score.js
@@ -3,16 +3,21 @@ import { useState, useEffect } from "react";
 
 export default function Score({teams}) {
     const [clickedTeams, setClickedTeams] = useState([]);
+    const [highScore, setHighScore] = useState(0);
 
     useEffect(() => {
         const updatedClickedTeams = teams.filter((team) => team.clicked === true);
         setClickedTeams(updatedClickedTeams);
-      }, [teams]);
+
+        if (updatedClickedTeams.length > highScore) {
+            setHighScore(updatedClickedTeams.length);
+          }
+      }, [teams, highScore]);
 
     return(
         <div className="scoreBoard">
             <div className="currentScore">{clickedTeams.length}</div>
-            <div className="highScore">10</div>
+            <div className="highScore">{highScore}</div>
         </div>
     )
 }
